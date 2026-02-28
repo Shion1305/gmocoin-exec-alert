79be84b99506fea791293f83d6f8b82be07869a9
Shion Ichikawa <shion1305@gmail.com>
2026-03-01 03:36:56 +0900

    ⬆️ websockets>=16.0

--- src/gmocoin_exec_alert/main.py
+++ src/gmocoin_exec_alert/main.py
@@ -7,6 +7,7 @@ import signal
 from typing import Any
 
 import websockets
+from websockets.asyncio.client import ClientConnection
 
 from .config import load_config
 from .dedup import DedupCache
@@ -78,7 +79,7 @@ async def _extend_token_loop(
 async def _recv_loop(
     *,
     stop: asyncio.Event,
-    ws: websockets.WebSocketClientProtocol,
+    ws: ClientConnection,
     channels: set[str],
     dedup: DedupCache,
     pd: PagerDutyClient,
